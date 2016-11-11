0faea61c33028daa490bfb038483006d39311abe
alsash <alsashcom@gmail.com>
2016-11-12 01:53:29 +0200

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawActivity.java
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.draganddraw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DragAndDrawActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
