4a3a13e32fa22c34995fe3ed2f61590ef8b7d62f
alsash <alsashcom@gmail.com>
2016-11-12 02:10:00 +0200

    Chapter 29

--- app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawActivity.java
+++ app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawActivity.java
@@ -1,9 +1,14 @@
 package com.bignerdranch.android.draganddraw;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
 
-public class DragAndDrawActivity extends AppCompatActivity {
+public class DragAndDrawActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return DragAndDrawFragment.newInstance();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
