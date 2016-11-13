4c979aa3c3e0bf6f516c5f98447fa1a5d6c0f3d6
alsash <alsashcom@gmail.com>
2016-11-13 23:25:41 +0200

    Chapter 29

--- app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawFragment.java
+++ app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawFragment.java
@@ -1,12 +1,12 @@
 package com.bignerdranch.android.draganddraw;
 
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class DragAndDrawFragment extends ListFragment {
+public class DragAndDrawFragment extends Fragment {
 
     public static DragAndDrawFragment newInstance() {
         return new DragAndDrawFragment();
